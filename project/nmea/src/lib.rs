8956cf7ae5a76a3a8c35aa1682b678f6a2b3ddc5
thaMilo <milougo.galli@gmail.com>
2025-01-11 17:24:40 +0100

    working on it

--- project/nmea/src/lib.rs
+++ project/nmea/src/lib.rs
@@ -1,13 +1,58 @@
 pub struct Nmea {
-    talker_id: String,
+    talker_id: TalkerId,
+    sentence: Sentence,
+}
+
+enum TalkerId {
+    AiAlarmIndicator,
+    ApAutoPilot,
+    BdBeidouChina,
+    CdDsc,
+    EcEcdis,
+    GaGalileoPs,
+    GbBeidouChina,
+    GiNavicIrnssIndia,
+    GlGlonassIEIC611621,
+    GnMultipleSatelliteSystem,
+    GpGlobalPositioningSystemReceiver,
+    GqQZSSRegionalGpsASJapan,
+    HcHeadingCompass,
+    HeGyroNorthSeeking,
+    IiIntegratedInstrumentation,
+    InIntegratedNavigation,
+    LcLorancReceiver,
+    PxxxProprietary,
+    PqQuectelQuirk,
+    QzQzssRegionalGpsASJapan,
+    SdDepthSounder,
+    StSkytraq,
+    TiTurnIndicator,
+    YxTransducer,
+    WiWeatherInstrument,
+    NotRecognized,
+}
+
+struct Sentence {
+    sentence_type: String,
 }
 
 impl Nmea {
     pub fn new() -> Self {
         Nmea {
-            talker_id: String::new(),
+            talker_id: TalkerId::NotRecognized,
+            sentence: Sentence {
+                sentence_type: String::new(),
+            },
         }
     }
 
-    pub fn parse(&self, sentence: String) -> Result<(), i32> {}
+    /*
+        TODO : parse talkerid
+        TODO : parse sentences : [ HDT , VDM, VDO, GGA, GLL, RMC, DPT, VHW, TTM, TLL, ZDA]
+        TODO : parse all other sentences or most of them anyway
+    */
+
+    pub fn parse(&self, _sentence: String) -> Result<(), i32> {
+        Ok(())
+    }
 }
